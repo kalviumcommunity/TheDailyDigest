627c11f98712f298b89f5eab098c7fb22abbb579
Kasinath S G <kasinathsg@gmail.com>
2023-03-10 17:03:26 +0530

    Front & Backend initialisation

---
+++ back-end/GizmodoFeed.js
@@ -0,0 +1,28 @@
+const app = require('express').Router()
+const axios = require('axios');
+const xml2js = require('xml2js')
+const parseString = xml2js.Parser({
+  tagNameProcessors: [xml2js.processors.stripPrefix]
+}).parseString;
+
+const feedUrl = 'https://gizmodo.com/rss';
+
+app.get('/gizmodo', (req, res) => {
+  axios.get(feedUrl)
+    .then(response => {
+      parseString(response.data, (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Error fetching feed');
+        }
+        return res.status(200).send(result)
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error fetching feed');
+    });
+});
+
+
+module.exports = app
---
+++ front-end/src/App.js
@@ -0,0 +1,37 @@
+import Navbar from './Components/Navbar';
+import { Route, Routes } from "react-router-dom"
+import Ars from './Components/ArsTechnica';
+import "./App.css"
+import Tnw from './Components/Tnw';
+import Eng from './Components/Engadget';
+import Venture from './Components/VentureBeat';
+import Giz from './Components/Gizmodo';
+import Digi from './Components/DigitalTrends';
+import Android from './Components/AndroidAuthority';
+import Verge from './Components/TheVerge';
+import Police from './Components/AndroidPolice';
+import Muo from './Components/Muo';
+
+function App() {
+  return (
+      <div className="container">
+        <Navbar/>
+        <Routes>
+          <Route path={"/arstechnica"} element={<Ars />} />
+          <Route path={"/tnw"} element={<Tnw />} />
+          <Route path={"/engadget"} element={<Eng/>}/>
+          <Route path={"/androidpolice"} element={<Police/>}></Route>
+          <Route path={"/venture"} element={<Venture/>}></Route>
+          <Route path={"/gizmodo"} element={<Giz/>}></Route>
+          <Route path={"/digitaltrends"} element={<Digi/>}></Route>
+          <Route path={"/androidauthority"} element={<Android/>}></Route>
+          <Route path={"/theverge"} element={<Verge/>}></Route>
+          <Route path={"/muo"} element={<Muo/>}></Route>
+        </Routes>
+        
+      </div>
+
+  );
+}
+
+export default App;
